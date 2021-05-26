f382a4fd8a08538e53c35bfb61bcaf5bd1e35da6
Nicolas Oudea <oudea.nicolas@gmail.com>
2021-05-26 14:31:55 +0200

    fixed offset x

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,6 @@ const Home = () => {
     <style jsx>{`
             .welcomeContainer {
                 height:100%;
-                width:100%;
                 display:flex;
                 flex-direction: column;
                 justify-content: center;
